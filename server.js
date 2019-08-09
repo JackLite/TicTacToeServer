82ac0a26de77fdb78a7ae5188382ed10b3242e88
Сергей Дючков <jackkdlite@gmail.com>
2019-08-09 17:10:31 +0300

    Ходы игроков и ники

--- server.js
+++ server.js
@@ -14,29 +14,47 @@ io.on("connection", (socket) => {
 
     players[playerId] = {
         id: playerId,
-        "socket": socket
+        "socket": socket, 
+        name: ""
     };
 
     socket.emit("successful connected", { id: playerId });
 
-    if (Object.keys(freePlayers).length < 1) {
-        freePlayers[playerId] = players[playerId];
-        socket.emit("waiting");
-    } else {
-        console.log("ready client: ", playerId);
-        const enemyId = Object.keys(freePlayers)[0];
-        socket.emit("enemy find", { id: enemyId });
-        players[enemyId].socket.emit("enemy find", { id: playerId });
-        const firstPlayerId = Math.round(Math.random()) == 1 ? enemyId : playerId;
-        socket.emit("first player", { id: firstPlayerId });
-        players[enemyId].socket.emit("first player", { id: firstPlayerId });
-        delete freePlayers[enemyId];
-    }
+    socket.on("player settings", (data) => {
+        players[playerId].name = data.nickname;
+    });
+
+    socket.on("player settings", (data) => {
+        players[playerId].name = data.nickname;
+
+        if (Object.keys(freePlayers).length < 1) {
+            freePlayers[playerId] = players[playerId];
+            socket.emit("waiting");
+        } else {
+            console.log("ready client: ", playerId);
+    
+            const enemyId = Object.keys(freePlayers)[0];
+            socket.emit("enemy find", { id: enemyId, nickname: players[enemyId].name });
+            players[enemyId].socket.emit("enemy find", { id: playerId, nickname: players[playerId].name });
+    
+            const firstPlayerId = Math.round(Math.random()) == 1 ? enemyId : playerId;
+            socket.emit("first player", { id: firstPlayerId });
+            players[enemyId].socket.emit("first player", { id: firstPlayerId });
+    
+            delete freePlayers[enemyId];
+        }
+    });
 
     socket.on("player step", (data) => {
         const enemySocket = players[data.enemyId].socket;
-        enemySocket.emit("enemy step", { position: data.position, state: data.state });
+        enemySocket.emit("enemy step", { position: data.position });
         console.log(data);
+    });
+
+    socket.on("end game", (data) => {
+        console.log("end game. winnerId: ", data.winnerId, " winner name: ", data.winnerName);
+        delete players[playerId];
+        delete freePlayers[playerId];
     });
 
     socket.on("disconnect", () => {
